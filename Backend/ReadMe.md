ec8c718f96123f3af7fc62406456eb03f5023551
François <francoislancastre@gmail.com>
2023-01-28 14:49:11 +0100

    Get works / Filters testing

--- Backend/ReadMe.md
+++ Backend/ReadMe.md
@@ -1,6 +1,6 @@
 # Backend API - Sophie Bluel
 
-Ce repo contient le code backend de l'architecte Sophie Bluel. 
+Ce repo contient le code backend de l'architecte Sophie Bluel.
 
 ## Lancement du backend
 
@@ -13,9 +13,12 @@ Compte de test pour Sophie Bluel
 ```
 email: sophie.bluel@test.tld
 
-password: S0phie 
+password: S0phie
 ```
+
 Lien pour voir la
 [documentation Swagger](http://localhost:5678/api-docs/)
 
 Pour lire la documentation, utiliser Chrome ou Firefox
+
+"token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY3NDQ4NzIwNCwiZXhwIjoxNjc0NTczNjA0fQ.0DMMiYzempjZoa3p5Jw17YUxJBHOLqY-i68-6AcqhlI"
---
+++ FrontEnd/index.js
@@ -0,0 +1,125 @@
+
+
+// fetch("http://localhost:5678/api/works").then((res) => res.json()).then((data) => {
+//     console.log(data);
+   
+  
+// })
+
+// Recupère les travaux et les affiche sur le dom 
+
+
+(async ()=> {
+
+    const reponse = await fetch('http://localhost:5678/api/works');
+    const works = await reponse.json();
+
+   
+    // console.log(works);
+    function getWorks(works) {
+
+        for (let i = 0; i < works.length; i++) {
+            
+            const work = works[i];
+            
+            // console.log(work);
+            
+            // On boucle pour afficher tout les traveaux 
+            
+            const sectionTest = document.querySelector(".test");
+            
+            const photoElement = document.createElement("article");
+            
+            const imageElement = document.createElement("img");
+            imageElement.src = work.imageUrl;
+            imageElement.crossOrigin = "anonymous";
+            imageElement.classList.add("testimg");
+            
+            const nomElement = document.createElement("p");
+            nomElement.innerText = work.title;
+            
+            sectionTest.appendChild(photoElement);
+            
+            photoElement.appendChild(imageElement);
+            photoElement.appendChild(nomElement);
+            
+            
+            
+            
+        }
+    }
+
+    getWorks(works);
+        
+        // ----------------------------------------------------------------/
+
+        // première versions sans boucle récuperation des travaux
+    // const imageElement = document.createElement("img");
+    // imageElement.src = work.imageUrl;
+    // imageElement.crossOrigin = "anonymous";
+    // imageElement.classList.add("testimg");
+
+    // const nomElement = document.createElement("p");
+    // nomElement.innerText = work.title;
+
+    // const sectionTest = document.querySelector(".test");
+
+    // sectionTest.appendChild(imageElement);
+    // sectionTest.appendChild(nomElement);
+
+
+    // ---------------------------------------------------------------
+        //  test de mapping
+    // const name = works.map(work => work.categoryId);
+    // console.log(name);
+
+
+    // config bouton tout 
+
+    
+    const boutonTout = document.querySelector(".btn-tout");
+boutonTout.addEventListener("click", () => {
+   document.querySelector(".test").innerHTML = "";
+   getWorks(works);
+});
+
+
+    // configuration bouton filtre Object
+
+    const boutonObjet = document.querySelector(".btn-objet");
+boutonObjet.addEventListener("click", () => {
+    const photosObjets = works.filter((work)=>{
+        return work.categoryId === 1;
+    });
+   document.querySelector(".test").innerHTML = "";
+   getWorks(photosObjets);
+});
+
+
+// configuration bouton filtre Appartements 
+
+const boutonAppartement = document.querySelector(".btn-appartement");
+boutonAppartement.addEventListener("click", () => {
+    const photosObjets = works.filter((work)=>{
+        return work.categoryId === 2;
+    });
+   document.querySelector(".test").innerHTML = "";
+   getWorks(photosObjets);
+});
+
+
+// Config bouton hotel resto 
+
+const boutonHotel = document.querySelector(".btn-hotel");
+boutonHotel.addEventListener("click", () => {
+    const photosObjets = works.filter((work)=>{
+        return work.categoryId === 3;
+    });
+   document.querySelector(".test").innerHTML = "";
+   getWorks(photosObjets);
+});
+
+
+
+})();
+
