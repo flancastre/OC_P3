d44875166c577862b51b3346a51b58c9d8558906
François <francoislancastre@gmail.com>
2023-02-05 19:52:07 +0100

    V2.2 Login pages

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -39,6 +39,12 @@ let mail, passwords;
 email.addEventListener("input", (e) => {
 
     mail = e.target.value;
+
+    if(mail === "sophie.bluel@test.tld") {
+        email.style.border = "solid green 1px";
+    } else {
+        email.style.border = "solid red 1px";
+    }
  
 });
 
@@ -46,6 +52,12 @@ password.addEventListener("input", (e) => {
 
     passwords = e.target.value;
  
+    if(passwords === "S0phie") {
+        password.style.border = "solid green 1px";
+    } else {
+        password.style.border = "solid red 1px";
+    }
+
 });
 
 form.addEventListener("submit", (e) => {
@@ -65,8 +77,11 @@ form.addEventListener("submit", (e) => {
         cretentials: "same-origin"
      })
  .then((res) => res.json())
- .then((data) => console.log(data))
+ .then((data) => window.localStorage.setItem("token", JSON.stringify(data.token)))
  .catch((error) => {console.log(error)});
+
+ 
+
 
 
 });
